45982c7e957c89d95cac774b0c9595c92cbbae81
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-08-21 23:46:38 +0900

    union

--- 2_union_and_intersection/1_union_basics.ts
+++ 2_union_and_intersection/1_union_basics.ts
@@ -1,21 +1,23 @@
 /**
  * Union Basics
- * 
- * 유니언은 TS에서 타입을 병합 할 수 있는 수많은 방법중 하나이다.
+ *
+ * 유니언은 TS에서 타입을 병합 할 수 있는 수많은 방법 중 하나이다. (제일 많이 사용됨)
+ *
+ * Or의 개념
  */
 type StringOrBooleanType = string | boolean;
 
-let stringOrBooleanType: StringOrBooleanType = '아이브';
+let stringOrBooleanType: StringOrBooleanType = "아이브";
 stringOrBooleanType = true;
 
-// stringOrBooleanType = undefined;
+// stringOrBooleanType = undefined; // Type 'undefined' is not assignable to type 'StringOrBooleanType'
 
 type StrBoolNullType = string | boolean | null;
 
-type StateTypes = 'DONE' | 'LOADING' | 'ERROR';
+type StateTypes = "DONE" | "LOADING" | "ERROR";
 
-let state: StateTypes = 'DONE';
-state = 'LOADING';
+let state: StateTypes = "DONE";
+state = "LOADING";
 // state = 'INITIAL';
 
 /**
@@ -25,38 +27,27 @@ state = 'LOADING';
 type StringListOrBooleanList = string[] | boolean[];
 
 let strListOrBooleanList: StringListOrBooleanList = [
-    '아이유',
-    '김고은',
-    '박소담',
-]
+  "아이유",
+  "김고은",
+  "박소담",
+];
 
-strListOrBooleanList = [
-    true,
-    false,
-    false,
-    true,
-]
+strListOrBooleanList = [true, false, false, true];
 
+// 두 타입을 쓰까먹을 수는 없다.
 // strListOrBooleanList = [
 //     true,
 //     '아이유',
 // ]
 
+// 이렇게 선언하면 두 타입을 쓰까서 어레이에 넣을 수 있음
 type StrOrNumberList = (string | number)[];
 
-let stringOrNumberList = [
-    1, 2, 3,
-    '아이유',
-    '레드벨벳',
-];
+let stringOrNumberList = [1, 2, 3, "아이유", "레드벨벳"];
 
-stringOrNumberList = [
-    1,2,3
-]
+stringOrNumberList = [1, 2, 3];
 
-stringOrNumberList = [
-    '아이유', '레드벨벳',
-]
+stringOrNumberList = ["아이유", "레드벨벳"];
 
 // stringOrNumberList = [
 //     true,
@@ -66,24 +57,24 @@ stringOrNumberList = [
 /**
  * Interface로 사용하는 union
  */
-interface Animal{
-    name: string;
-    age: number;
+interface Animal {
+  name: string;
+  age: number;
 }
 
 interface Human {
-    name: string;
-    age: number;
-    address: string;
+  name: string;
+  age: number;
+  address: string;
 }
 
 type AnimalOrHuman = Animal | Human;
 
 let animalOrHuman: AnimalOrHuman = {
-    name: '최지호',
-    age: 32,
-    address: '대한민국',
-}
+  name: "최지호",
+  age: 32,
+  address: "대한민국",
+};
 
 console.log(animalOrHuman);
 console.log(animalOrHuman.name);
@@ -91,27 +82,28 @@ console.log(animalOrHuman.age);
 console.log(animalOrHuman.address);
 
 animalOrHuman = {
-    name: '오리',
-    age: 9,
-}
+  name: "오리",
+  age: 9,
+};
 
 console.log(animalOrHuman);
-
 console.log(animalOrHuman.name);
 console.log(animalOrHuman.age);
-// console.log(animalOrHuman.address);
+// console.log(animalOrHuman.address); // Property 'address' does not exist on type 'Animal'.
 
-let animalOrHuman2: {
-    name: string;
-    age: number;
-} | {
-    name: string;
-    age: number;
-    address: string;
-} ={
-    name: '최지호',
-    age: 32,
-    address: '대한민국',
+let animalOrHuman2:
+  | {
+      name: string;
+      age: number;
+    }
+  | {
+      name: string;
+      age: number;
+      address: string;
+    } = {
+  name: "최지호",
+  age: 32,
+  address: "대한민국",
 };
 
 console.log(animalOrHuman2.address);
@@ -119,8 +111,8 @@ console.log(animalOrHuman2.name);
 console.log(animalOrHuman2.age);
 
 animalOrHuman2 = {
-    name: '오리',
-    age: 9,
+  name: "오리",
+  age: 9,
 };
 
 // console.log(animalOrHuman2.address);
@@ -129,20 +121,23 @@ console.log(animalOrHuman2.age);
 
 // 서로 관계가 없는 유니언을 선언하면 어떻게 되는가
 type Person = {
-    name: string;
-    age: number;
-}
+  name: string;
+  age: number;
+};
 
 type Cat = {
-    breed: string;
-    country: string;
-}
+  breed: string;
+  country: string;
+};
 
 type PersonOrCat = Person | Cat;
 
+// 각각 선언하는 것과 두 타입 모두 선언하는거 괜찮은데, 각타입에서 하니씩 선언하는건 또 안됨. 근데 이렇게 쓸 일이 있나?
 const personOrCat: PersonOrCat = {
-    // name: '코드팩토리',
-    age: 32,
-    // breed: 'Yorkshire Terrier',
-    country: '영국',
-}
+  name: "코드팩토리",
+  age: 32,
+  breed: "Yorkshire Terrier",
+  country: "영국",
+};
+
+console.log(personOrCat);
--- 2_union_and_intersection/2_intersection_basics.ts
+++ 2_union_and_intersection/2_intersection_basics.ts
@@ -1,27 +1,28 @@
 /**
  * Intersection
- * 
- * And
+ *
+ * And의 개념
  */
-interface Human{
-    name: string;
-    age: number;
+interface Human {
+  name: string;
+  age: number;
 }
 
-interface Contacts{
-    phone: string;
-    address: string;
+interface Contacts {
+  phone: string;
+  address: string;
 }
 
 type HumanAndContacts = Human & Contacts;
 
 let humanAndContacts: HumanAndContacts = {
-    name: '코드팩토리',
-    age: 32,
-    phone: '01012341234',
-    address: '서울시'
+  name: "코드팩토리",
+  age: 32,
+  phone: "01012341234",
+  address: "서울시",
 };
 
+// primitive type을 intersection 할 수 없다.
 type NumberAndString = number & string;
 
-// let numberAndString: NumberAndString = never;
+// let numberAndString: NumberAndString = never; // never는 ts의 타입이지 js의 타입은 아니다. 따라서 할당 불가능
