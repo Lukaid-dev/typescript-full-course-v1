1bfe19737d30b667c2970ba08762bd717858a58c
Seongwoo_Lukaid <crescent70004536@gmail.com>
2023-08-27 17:43:09 +0900

    type and interface

--- 4_type_and_interface/1_type_vs_interface.ts
+++ 4_type_and_interface/1_type_vs_interface.ts
@@ -1,21 +1,24 @@
 /**
  * Type vs Interface
+ *
+ * Type이 먼저 개발되고, 이를 보완하기 위해 Interface가 나왔다.
+ * 하지만, Interface가 Type을 대체할 수 있는 것은 아니다.
  */
 
 // Object 선언할때
 interface IObject {
-    x: number;
-    y: number;
+  x: number;
+  y: number;
 }
 
 type TObject = {
-    x: number;
-    y: number;
-}
+  x: number;
+  y: number;
+};
 
 // function을 선언할때
 interface IFunction {
-    (x: number, y: number): number;
+  (x: number, y: number): number;
 }
 
 type TFunction = (x: number, y: number) => number;
@@ -39,20 +42,23 @@ type TupleType = [number, string];
  * Type은 못하는 것
  */
 
-// interface merging
+// interface merging, interface를 합치는거 *중요*
+// interface는 같은 이름으로 여러번 선언할 수 있다.
+// 중복선언 했을때, 같은 이름의 interface를 합쳐서 사용할 수 있다.
 interface IRectangle {
-    height: number;
+  height: number;
 }
 
 interface IRectangle {
-    width: number;
+  width: number;
 }
 
 let rectangle: IRectangle = {
-    height: 200,
-    width: 100,
-}
+  height: 200,
+  width: 100,
+};
 
+// Duplicate identifier 'TRectangle'.
 // type TRectangle = {
 //     height: number;
 // }
@@ -65,40 +71,42 @@ let rectangle: IRectangle = {
  * Interface Merging
  */
 class Review {
-    // 프로퍼티
-    getY = (x: number) => { return x };
+  // 프로퍼티, instance에 귀속된다.
+  getY = (x: number) => {
+    return x;
+  };
 
-    // 메서드
-    getX(x: number) {
-        return x;
-    }
+  // 메서드, prototype에 귀속된다.
+  getX(x: number) {
+    return x;
+  }
 }
 
 interface GetXnY {
-    getX: (x: number) => number;
-    getY: (y: number) => number;
+  getX: (x: number) => number;
+  getY: (y: number) => number;
 }
 
 interface GetXnY {
-    getX: (x: number) => number;
-    // getY: (y: string) => number;
+  getX: (x: number) => number;
+  // getY: (y: string) => number;
 }
 
 interface GetXnY2 {
-    getX(x: number): number;
-    getY(y: number): number;
+  getX(x: number): number;
+  getY(y: number): number;
 }
 
 interface GetXnY2 {
-    getX(x: number): number;
-    getY(y: string): number;
+  getX(x: number): number;
+  getY(y: string): number; // 함수는 오버로딩이 됨
 }
 
 const testMethod: GetXnY2 = {
-    getX(x){
-        return x;
-    },
-    getY(y){
-        return 1;
-    }
-}
+  getX(x) {
+    return x;
+  },
+  getY(y) {
+    return 1;
+  },
+};
--- 4_type_and_interface/2_extension.ts
+++ 4_type_and_interface/2_extension.ts
@@ -1,121 +1,123 @@
 /**
  * Extension
  */
-interface IName{
-    name: string;
+interface IName {
+  name: string;
 }
 
-interface IIdol extends IName{
-    age: number;
+interface IIdol extends IName {
+  age: number;
 }
 
 const idol: IIdol = {
-    name: '안유진',
-    age: 23,
-}
+  name: "안유진",
+  age: 23,
+};
 
 type TName = {
-    name: string;
-}
+  name: string;
+};
 
 type TIdol = TName & {
-    age: number
+  age: number;
 };
 
 const idol2: TIdol = {
-    name: '아이유',
-    age: 29,
-}
+  name: "아이유",
+  age: 29,
+};
 
-interface IIdol2 extends TName{
-    age: number;
+// interface는 type을 상속받을 수 있다.
+interface IIdol2 extends TName {
+  age: number;
 }
 
 const idol3: IIdol2 = {
-    name: '제니',
-    age: 29,
-}
+  name: "제니",
+  age: 29,
+};
 
+// type은 interface를 상속받을 수 있다.
 type TIdol2 = IName & {
-    age: number;
-}
+  age: number;
+};
 
 const idol4: TIdol2 = {
-    name: '지수',
-    age: 31,
-}
+  name: "지수",
+  age: 31,
+};
 
 /**
  * extending multiple
  */
 type DogName = {
-    name: string;
-}
+  name: string;
+};
 
 type DogAge = {
-    age: number;
-}
+  age: number;
+};
 
 type DogBreed = {
-    breed: string;
-}
+  breed: string;
+};
 
 type Dog = DogName & DogAge & DogBreed;
 
 const dog: Dog = {
-    name: '코드팩토리',
-    age: 32,
-    breed: 'Poodl',
-}
+  name: "코드팩토리",
+  age: 32,
+  breed: "Poodle",
+};
 
-interface CatName{
-    name: string;
+interface CatName {
+  name: string;
 }
-interface CatAge{
-    age: number;
+interface CatAge {
+  age: number;
 }
-interface Cat extends CatName, CatAge{
-    breed: string;
+interface Cat extends CatName, CatAge {
+  breed: string;
 }
 
 const cat: Cat = {
-    name: '오리',
-    age: 7,
-    breed: '코리안 냥냥이',
-}
+  name: "오리",
+  age: 7,
+  breed: "코리안 냥냥이",
+};
 
 /**
- * Overrding
+ * Overriding
  */
 type THeight = {
-    height: number;
-}
+  height: number;
+};
 
 type TRectangle = THeight & {
-    height: string;
-    width: number;
-}
+  height: string; // primitive type을 intersection하면 never가 된다.
+  width: number;
+};
 
 // const rectangle: TRectangle = {
-//     height:,
+//     height:,  //  height는 never가 된다.
 //     width: 100,
 // }
 
 type TWidth = {
-    width: number | string;
-}
+  width: number | string;
+};
 type TRectangle2 = TWidth & {
-    width: number;
-    height: number;
-}
+  width: number; // 이 경우에는 하나의 type으로 정의된다. narrowing이 일어난다.
+  height: number;
+};
 
 const rectangle: TRectangle2 = {
-    height: 100,
-    width: 200,
-}
+  height: 100,
+  width: 200,
+};
 
-interface IHeight{
-    height: number;
+interface IHeight {
+  height: number;
 }
 
 // interface IRectangle extends IHeight{
@@ -123,11 +125,11 @@ interface IHeight{
 //     width: number;
 // }
 
-interface IWidth{
-    width: number | string;
+interface IWidth {
+  width: number | string;
 }
 
-interface IRectangle extends IWidth{
-    width: number;
-    height: number;
+interface IRectangle extends IWidth {
+  width: number;
+  height: number;
 }
